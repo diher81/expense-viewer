a8df3e66c9b3b9999f21ff85a59bff72e7060820
¨diher81¨ <hermans.dirk@hotmail.com>
2019-08-29 16:53:20 +0200

    initial commit

---
+++ src/main/java/be/diher81/expenseviewer/ExpenseviewerApplication.java
@@ -0,0 +1,13 @@
+package be.diher81.expenseviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseviewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/diher81/expenseviewer/ExpenseviewerApplicationTests.java
@@ -0,0 +1,16 @@
+package be.diher81.expenseviewer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExpenseviewerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
