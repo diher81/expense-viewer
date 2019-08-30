8fc6dd1e79d262386a0cfef92c21bfc0e4dc33b2
¨diher81¨ <hermans.dirk@hotmail.com>
2019-08-30 20:45:49 +0200

    add a simple ExpenseService

---
+++ src/main/java/be/diher81/expenseviewer/service/ExpenseServiceImpl.java
@@ -0,0 +1,13 @@
+package be.diher81.expenseviewer.service;
+
+import be.diher81.expenseviewer.entity.Expense;
+
+import java.util.List;
+
+public class ExpenseServiceImpl implements ExpenseService {
+
+    @Override
+    public List<Expense> findAllExpenses() {
+        return null;
+    }
+}
