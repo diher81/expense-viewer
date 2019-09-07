81500ea39c23e76282981628a51f8d7d48742114
¨diher81¨ <hermans.dirk@hotmail.com>
2019-09-07 13:16:44 +0200

    add jpa annotation

--- src/main/java/be/diher81/expenseviewer/entity/Expense.java
+++ src/main/java/be/diher81/expenseviewer/entity/Expense.java
@@ -1,5 +1,6 @@
 package be.diher81.expenseviewer.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -10,6 +11,8 @@ public class Expense {
     @Id
     @GeneratedValue
     private int id;
+
+    @Column
     private String description;
 
 
