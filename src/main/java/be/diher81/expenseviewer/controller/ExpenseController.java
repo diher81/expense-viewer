742e2240e068062147ec61849b60fdeceeaa5030
¨diher81¨ <hermans.dirk@hotmail.com>
2019-09-07 13:30:02 +0200

    add a requestmapping to the controller

--- src/main/java/be/diher81/expenseviewer/controller/ExpenseController.java
+++ src/main/java/be/diher81/expenseviewer/controller/ExpenseController.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/expenses")
 public class ExpenseController {
 
     @Autowired
