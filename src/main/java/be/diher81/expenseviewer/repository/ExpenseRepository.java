80a90da54e857d032884459f90ff075f75a30a36
¨diher81¨ <hermans.dirk@hotmail.com>
2019-09-07 12:04:37 +0200

    add repository annotation

--- src/main/java/be/diher81/expenseviewer/repository/ExpenseRepository.java
+++ src/main/java/be/diher81/expenseviewer/repository/ExpenseRepository.java
@@ -2,9 +2,11 @@ package be.diher81.expenseviewer.repository;
 
 import be.diher81.expenseviewer.entity.Expense;
 import org.springframework.data.repository.Repository;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public interface ExpenseRepository extends Repository<Expense, Long> {
 
     List<Expense> findAllExpenses();
