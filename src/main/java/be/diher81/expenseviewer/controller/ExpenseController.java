cddb2185a395a3ec4e1baf3466301a9e91afd957
¨diher81¨ <hermans.dirk@hotmail.com>
2019-09-07 12:09:44 +0200

    add an initial controller

---
+++ src/main/java/be/diher81/expenseviewer/controller/ExpenseController.java
@@ -0,0 +1,20 @@
+package be.diher81.expenseviewer.controller;
+
+import be.diher81.expenseviewer.service.ExpenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ExpenseController {
+
+    @Autowired
+    private ExpenseService expenseService;
+
+    @GetMapping
+    public ResponseEntity findAllExpenses() {
+        return ResponseEntity.ok(expenseService.findAllExpenses());
+    }
+
+}
