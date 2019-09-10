367e25b200d852082105ccfd53852d47fe94afb9
¨diher81¨ <hermans.dirk@hotmail.com>
2019-09-10 23:49:10 +0200

    add a Table annotation to entity class expenses

--- src/main/java/be/diher81/expenseviewer/entity/Expense.java
+++ src/main/java/be/diher81/expenseviewer/entity/Expense.java
@@ -1,12 +1,10 @@
 package be.diher81.expenseviewer.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "expenses")
 public class Expense {
 
     @Id
