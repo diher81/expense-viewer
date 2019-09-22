6c03d28c0176ebab9508ae71891b011c5d52cfbc
¨diher81¨ <hermans.dirk@hotmail.com>
2019-09-22 17:45:33 +0200

    change class access modifier, add RestController annotation and add a GET mapping

--- src/main/java/be/diher81/expenseviewer/controller/ExpenseController.java
+++ src/main/java/be/diher81/expenseviewer/controller/ExpenseController.java
@@ -3,19 +3,19 @@ package be.diher81.expenseviewer.controller;
 import be.diher81.expenseviewer.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/expenses")
-public class ExpenseController {
+class ExpenseController {
 
     @Autowired
     private ExpenseService expenseService;
 
-    @GetMapping
-    public ResponseEntity findAllExpenses() {
+    @GetMapping("/findall")
+    public ResponseEntity<Object> findAllExpenses() {
         return ResponseEntity.ok(expenseService.findAllExpenses());
     }
 }
